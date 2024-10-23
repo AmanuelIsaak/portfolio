3f9fe6c5d805d6a0ebfd280a7b3ad2993dea1b6d
amanafro <diazermando193@gmail.com>
2024-10-23 18:00:29 +0200

    removed images from project cards and some other changes

--- src/components/text.js
+++ src/components/text.js
@@ -1,5 +1,3 @@
-<script>
-
 let context = {
   curiosity: {
     short:
@@ -12,54 +10,7 @@ let context = {
     long: "In 2021 I decided to enter <a class=\"font-medium text-blue-600 dark:text-blue-500 hover:underline\" href=\"https://wg.edubs.ch/schulprofil/ims\">IMS</a> in order to learn about software engineering. Meanwhile I did some research and wrote some 'toy-application' like calculators and password generators. Even though I was building something simple, it really felt like I've built something big and hard.Fast forward to 2022 and I was enrolled in IMS and there were just too many topics that we've learned about. Databases and building static websites with HTML and CSS. These were the main topics at that time. But suddenly I was scared to make some progress like start using Javascript. But it wasn't long before I overcame it.But nevertheless, I aspire to become a software engineer with passion and a deeper level of understanding in order to provide valuable work!",
   },
   whereDoIStandNow: {
-    short: "I have programming experience mainly in Go and JavaScript (Svelte), with school exposure to PHP and Java. I've built some simple websites and decent apps but still feel like a beginner. I lack a deep understanding of software fundamentals, data structures, algorithms, and haven't developed a complex application that covers both frontend and backend, along with topics like security, networking, and design patterns.",
+    short: "Short description of education.",
     long: "At this point I have some experience at programming in general. I mostly used Goland and Javascript, Svelte specifically, and in school we mostly used PHP and Java. I've build some silly websites but I also build some decent apps as well. But I still feel like a beginner since I have no deep understanding of how software actually works under the hood. I still don't know much about Data structures and Algorithms, I still haven't build a complex application to really cover frontend and backend and just other skills and topics like security, network, design pattern etc.",
   },
 };
-</script>
-
-<ol class="relative border-s border-gray-200 dark:border-gray-700">
-  <li class="mb-10 ms-4">
-    <div
-      class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
-    ></div>
-    <time
-      class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-    >>=2021</time
-    >
-    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-      Curiosity
-    </h3>
-    <p class="text-base font-normal text-gray-500 dark:text-gray-400 block md:hidden">{@html context.curiosity.short}</p>
-    <p class="text-base font-normal text-gray-500 dark:text-gray-400 hidden md:block">{@html context.curiosity.long}</p>
-  </li>
-
-  <li class="mb-10 ms-4">
-    <div
-      class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
-    ></div>
-    <time
-      class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-    >2021-2022</time
-    >
-    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-      First steps
-    </h3>
-    <p class="text-base font-normal text-gray-500 dark:text-gray-400 block md:hidden">{@html context.firstStep.short}</p>
-    <p class="text-base font-normal text-gray-500 dark:text-gray-400 hidden md:block">{@html context.firstStep.long}</p>
-  </li>
-  <li class="ms-4">
-    <div
-      class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
-    ></div>
-    <time
-      class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-    >2022-now</time
-    >
-    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-      Where do I stand now?
-    </h3>
-    <p class="text-base font-normal text-gray-500 dark:text-gray-400 block md:hidden">{@html context.whereDoIStandNow.short}</p>
-    <p class="text-base font-normal text-gray-500 dark:text-gray-400 hidden md:block">{@html context.whereDoIStandNow.long}</p>
-  </li>
-</ol>
